f5d73c0c09cc365605787972a45c390a072e6dfc
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-28 12:22:12 +0530

    OPPE here I come

--- 4thquestion.py
+++ 4thquestion.py
@@ -4,3 +4,14 @@
 
 # when it is only divisible by 1 and the number itself
 # only then we can say that the number is prime 
+
+def primeChecker (num : int):
+    # numberroot = int (num ** 0.5)
+    for i in range (2, num):
+        if (num % i == 0) :
+            return (f'{num} is not a prime number')
+        
+    return f'{num} is a prime number'
+    # # return numberroot
+
+print (primeChecker(int (17)))
