66a4c5e9bdfeac97b8318cd84291ea2973e15e80
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-01 18:26:04 +0530

    pythoning was pythoning

--- learning/tuple.py
+++ learning/tuple.py
@@ -3,3 +3,10 @@
 # tuple -> (2, 3, 4) comes with closed brackets
 # lists -> [2, 3, 4] comes with square brackets
 
+tuple1 = (2, 3, 4)
+
+list1 = [(91, 92 , 93), 61, 79]
+
+print (tuple1[0])
+
+print (list1[0][1])
