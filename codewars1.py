a8c90de497f2eee5df9782045780f071bd1f65ce
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-01 09:19:30 +0530

    pythoning was pythoning

--- codewars1.py
+++ codewars1.py
@@ -1,4 +1,4 @@
-#A Narcissistic Number (or Armstrong Number) is a positive number
+# A Narcissistic Number (or Armstrong Number) is a positive number
 # which is the sum of its own digits, each raised to the power of
 # the number of digits in a given base. In this Kata, we will 
 # restrict ourselves to decimal (base 10).
